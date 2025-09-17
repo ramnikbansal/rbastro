fdf9a46fc417110b4f06e17f79b9d96ef6826ca7
ramnikbansal <ramnik.bansal@gmail.com>
2025-09-17 22:35:06 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-# Force reinstall swisseph in case of build issues
+
+# Upgrade pip just in case
+pip install --upgrade pip
+
+# Force install swisseph and dependencies
 pip install --no-cache-dir --force-reinstall swisseph
+pip install --no-cache-dir -r requirements.txt
